28dc051f393061d6061812bfc8d1a0e9ceb17642
sam muigai <muigai.samuel21@students.dkut.ac.ke>
2023-08-25 16:16:11 +0300

    Added timber to ease debugging

--- app/src/main/java/com/samkt/gameify/GameifyApplication.kt
+++ app/src/main/java/com/samkt/gameify/GameifyApplication.kt
@@ -2,6 +2,17 @@ package com.samkt.gameify
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class GameifyApplication : Application()
+class GameifyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        Timber.plant(Timber.DebugTree())
+    }
+}
